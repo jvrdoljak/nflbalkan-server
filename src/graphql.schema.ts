d2fc53c31491b431fe74cd7750c0aa2a881a6937
josip-vrdoljak <josip.vrdoljak@pseudocode.agency>
2022-02-07 13:09:48 +0100

    First mutation

--- src/graphql.schema.ts
+++ src/graphql.schema.ts
@@ -7,17 +7,42 @@
 
 /* tslint:disable */
 /* eslint-disable */
-export class Post {
-    __typename?: 'Post';
+export class ImageInput {
+    src: string;
+    alt: string;
+}
+
+export class PostInput {
     id: number;
     title: string;
     text: string;
+    image?: Nullable<ImageInput>;
+}
+
+export class ImageResult {
+    __typename?: 'ImageResult';
+    src: string;
+    alt: string;
+}
+
+export class PostResult {
+    __typename?: 'PostResult';
+    id: number;
+    title: string;
+    text: string;
+    image?: Nullable<ImageResult>;
 }
 
 export abstract class IQuery {
     __typename?: 'IQuery';
 
-    abstract post(id: number): Post | Promise<Post>;
+    abstract post(id: number): PostResult | Promise<PostResult>;
+}
+
+export abstract class IMutation {
+    __typename?: 'IMutation';
+
+    abstract createPost(post: PostInput): PostResult | Promise<PostResult>;
 }
 
 type Nullable<T> = T | null;
--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,9 +1,15 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { PostInput } from 'src/graphql.schema';
 
 @Resolver('Posts')
 export class PostsResolver {
   @Query()
   async post(@Args('id') id: number) {
-    return { id: id, text: 'Test', title: 'Test' };
+    return { id: id, text: 'Test', title: 'Test', image: { src: '', alt: '' } };
+  }
+
+  @Mutation()
+  async createPost(@Args('post') post: PostInput) {
+    return post;
   }
 }
